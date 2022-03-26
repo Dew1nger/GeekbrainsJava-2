f152e8e44c538df35d221a771e93fae53c912063
Dew1nger <lazarev.vadim96@gmail.com>
2022-03-26 21:20:28 +0300

    Initial commit

---
+++ src/test/java/ru/Dew1nger/AppTest.java
@@ -0,0 +1,20 @@
+package ru.Dew1nger;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
